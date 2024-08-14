b3bc0e4d9bb23f769f160d33ec3409e8d605bcc1
Yao Zhang <60941643+yancy-zh@users.noreply.github.com>
2024-08-14 20:40:05 +0800

    Url parser worked

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # FrontEnd Top Interviewed Solutions
 
 A JavaScript project (Node.js + React) that offers samples solutions to the questions from Leetbook.
+
 ## Dependency
+
 - [AntD](https://)
+
 ## Description
 
-Answer questions from common interview questions published by Leetbook [Interview coding frontend 面试尊享100题](https://leetcode.cn/leetbook/read/interview-coding-frontend/dqouvi/)
+Answer questions from common interview questions published by Leetbook [Interview coding frontend 面试尊享 100 题](https://leetcode.cn/leetbook/read/interview-coding-frontend/dqouvi/)
 
 ## How to create a JavaScript project
 
@@ -20,4 +23,10 @@ In the root folder of the project, type the following in the command line:
 
 ```
 npm start
+```
+
+## Push to git repository
+
+```
+git push -u origin master
 ```
--- src/public/components/ParseUrl.js
+++ src/public/components/ParseUrl.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ParseUrl.css";
-import { Input, Button, Tooltip, UserOutlined, InfoCircleOutlined } from "antd";
+import { Input, Button, Space } from "antd";
 import { EditOutlined } from "@ant-design/icons";
+import JsonDisplay from "./JsonDisplay";
 export default function ParseUrl() {
+  const [inputVal, setInputVal] = useState("");
+  const [outputVal, setOutputVal] = useState("");
+  function queryURLparamsRegEs5(url) {
+    let obj = {};
+    let reg = /([^?=&]+)=([^?=&]+)/g;
+    url.replace(reg, (...arg) => {
+      obj[arg[1]] = arg[2];
+    });
+    return obj;
+  }
+  const handleClick = (e) => {
+    let obj = queryURLparamsRegEs5(inputVal);
+    for (const [key, value] of Object.entries(obj)) {
+      console.log(`${key}: ${value}`);
+    }
+    setOutputVal(obj);
+  };
+  const handleChange = (e) => {
+    setInputVal(e.target.value);
+  };
   return (
     <div className="parse-url-box">
       <div className="parse-url-input-wrapper">
-        <h4>Url string</h4>
+        <h4>Url String</h4>
         <Input.Group compact>
           <Input
             size="medium"
             style={{ width: "calc(100% - 200px)" }}
             prefix={<EditOutlined />}
             placeholder="Insert a url string"
+            onChange={handleChange}
           />
-          <Button type="primary">Submit</Button>
+          <Button type="primary" onClick={handleClick}>
+            Parse
+          </Button>
         </Input.Group>
-        <h4>parsed Url components</h4>
-        <Input.Group compact style={{ marginBottom: "20px" }}>
-          <Input
-            size="medium"
-            style={{ width: "80%" }}
-            placeholder="parsed url"
-          />
-        </Input.Group>
+        <h4>Parsed Url Components</h4>
+        <JsonDisplay data={outputVal}></JsonDisplay>
       </div>
     </div>
   );
